f12471c28209a8d98471a08f06c9d3fc6af67b89
isabelemara <isabele.m@aluno.ifsc.edu.br>
2024-04-10 22:07:22 -0300

    ok

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -22,7 +22,7 @@ typedef struct lista {
 lista_p* criar_p(int elemento);
 int inserir_p(lista_p *lista, contato_t* elemento);
 int imprimir_p(lista_p* lista);
-int buscar_lista(lista_p* lista, char *elemento);
+int buscar_contato(lista_p *lista, char nome[100]);
 bool excluir_p(lista_p *lista, char* elemento);
 void editar_p(lista_p *lista, int posicao,char *  nome,char* telefone, char * email);
 //void opcao(lista_p *lista);
