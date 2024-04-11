98832bda8b2d97f0b8e158402c100bd415378f2d
isabelemara <isabele.m@aluno.ifsc.edu.br>
2024-04-10 22:24:01 -0300

    ok

--- libprg/src/libprg/lista_projeto.c
+++ libprg/src/libprg/lista_projeto.c
@@ -3,15 +3,6 @@
 #include <stdbool.h>
 #include <string.h>
 #include <libprg/libprg.h>
-int buscar_contato(lista_p *lista, char nome[100]) {
-    int contagem = 0;
-    for (int i = 0; i < lista->tamanho; ++i) {
-        if (strcmp(lista->elemento[i].nome, nome) == 0) {
-            contagem++;
-        }
-    }
-    return contagem;
-}
 
 lista_p* criar_p(int capacidade) {
     lista_p *lista = (lista_p*)malloc(sizeof(lista_p));
@@ -40,26 +31,40 @@ int inserir_p(lista_p *lista, contato_t *elemento) {
     lista->tamanho++;
 }
 
-int buscar_lista(lista_p *lista, char *elemento) {
-    int inicio = 0;
-    int fim = lista->tamanho - 1;
-    int meio;
-    while (inicio <= fim) {
-        meio = (inicio + fim) / 2;
-        int comparar = strcmp(lista->elemento[meio].nome, elemento);
-        if (comparar == 0) {
-            return meio;
-        } else if (comparar < 0) {
-            inicio = meio + 1;
-        } else {
-            fim = meio - 1;
+
+
+
+
+int buscar_contato(lista_p *lista, char *nome) {
+    int contagem = 0;
+    for (int i = 0; i < lista->tamanho; ++i) {
+        // Verifica se o nome fornecido é uma substring do nome do contato
+        if (strstr(lista->elemento[i].nome, nome) != NULL) {
+            contagem++;
         }
     }
-    return -1;
+    return contagem;
 }
+//int buscar_lista(lista_p *lista, char *elemento) {
+//    int inicio = 0;
+//    int fim = lista->tamanho - 1;
+//    int meio;
+//    while (inicio <= fim) {
+//        meio = (inicio + fim) / 2;
+//        int comparar = strcmp(lista->elemento[meio].nome, elemento);
+//        if (comparar == 0) {
+//            return meio;
+//        } else if (comparar < 0) {
+//            inicio = meio + 1;
+//        } else {
+//            fim = meio - 1;
+//        }
+//    }
+//    return -1;
+//}
 
 bool excluir_p(lista_p *lista, char *elemento) {
-    int buscar = buscar_lista(lista, elemento);
+    int buscar = buscar_contato(lista, elemento);
     for (int i = buscar; i < lista->tamanho - 1; i++) {
         lista->elemento[i] = lista->elemento[i + 1];
         return true;
