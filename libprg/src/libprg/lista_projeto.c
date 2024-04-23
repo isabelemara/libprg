5bfd2d109d60fea2208a21cf9ff771c8e3026ce3
isabelemara <isabele.m@aluno.ifsc.edu.br>
2024-04-23 09:12:13 -0300

    modifiquei a busca e o excluir

--- libprg/src/libprg/lista_projeto.c
+++ libprg/src/libprg/lista_projeto.c
@@ -28,27 +28,17 @@ int inserir_p(lista_p *lista, contato_t *elemento) {
     lista->elemento[posicao] = *elemento;
     lista->tamanho++;
 }
-int* buscar_lista(lista_p *lista, char *nome) {
-    int* posicoes_encontradas = malloc(lista->tamanho * sizeof(int)); // Aloca memória para armazenar todas as posições
-    if (posicoes_encontradas == NULL) {
-
-        exit(EXIT_FAILURE);
-    }
-
-    int contagem = 0;
-
+int buscar_lista(lista_p *lista, char *nome) {
+    int posicao_encontrada = -1; // Inicializa com -1 para indicar que o nome não foi encontrado
     for (int i = 0; i < lista->tamanho; ++i) {
         if (strcasestr(lista->elemento[i].nome, nome) != NULL) {
-            posicoes_encontradas[contagem] = i;
-            contagem++;
+            posicao_encontrada = i;
+            break; // Se encontrar, não há necessidade de continuar a busca
         }
     }
-
-    // Armazena a quantidade de contatos encontrados na primeira posição
-    posicoes_encontradas[0] = contagem;
-
-    return posicoes_encontradas;
+    return posicao_encontrada;
 }
+
 
 
 bool excluir_p(lista_p *lista, int pessoa) {
