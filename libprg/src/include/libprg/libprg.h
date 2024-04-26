a38b569aa38c8bf8ea34a4f924ce3ac7b2aa7b70
isabelemara <isabele.m@aluno.ifsc.edu.br>
2024-04-26 13:18:40 -0300

    ok

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -27,7 +27,7 @@ int imprimir_p(lista_p* lista);
 int buscar_lista(lista_p *lista, char *nome) ;
 int excluir_p(lista_p *lista, int posicao) ;
 void editar_p(lista_p *lista, int posicao,char *  nome,char* telefone, char * email);
-void salvar_binario(lista_p * lista);
+void salvar_binario(lista_p * contato);
 void carregar_contatos(lista_p * contato);
 //void opcao(lista_p *contato);
 
