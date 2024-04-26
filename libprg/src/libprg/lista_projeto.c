44964b83debe03198092a7e0f187c1a190f60c25
isabelemara <isabele.m@aluno.ifsc.edu.br>
2024-04-26 13:06:30 -0300

    ok

--- libprg/src/libprg/lista_projeto.c
+++ libprg/src/libprg/lista_projeto.c
@@ -88,8 +88,8 @@ void editar_p(lista_p *lista, int posicao,char *  nome,char* telefone, char * em
         }
 
 
-        fwrite(&lista->tamanho, sizeof(int), 1, arquivo);
-        fwrite(lista->elemento, sizeof(lista_p), lista->tamanho, arquivo);
+        fwrite(&lista->capacidade, sizeof(int), 1, arquivo);
+        fwrite(lista->elemento, sizeof(contato_t), lista->capacidade, arquivo);
 
         fclose(arquivo);
     }
@@ -100,8 +100,8 @@ void editar_p(lista_p *lista, int posicao,char *  nome,char* telefone, char * em
             return;
         }
 
-        fread(&lista->tamanho, sizeof(int), 1, arquivo);
-        fread(lista->elemento, sizeof(lista_p), lista->tamanho, arquivo);
+        fread(&lista->capacidade, sizeof(int), 1, arquivo);
+        fread(lista->elemento, sizeof(contato_t), lista->capacidade, arquivo);
 
         fclose(arquivo);
     }
