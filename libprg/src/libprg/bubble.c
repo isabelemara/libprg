c008bc3f211373576b10350829754a0986e6757c
isabele.m <isabele.m@aluno.ifsc.edu.br>
2024-07-08 11:03:07 -0300

    ok

---
+++ libprg/src/libprg/bubble.c
@@ -0,0 +1,15 @@
+#include <libprg/libprg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void bubblesort(int vetor[], int n){
+    int i, j, temp;
+
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+
+        }
+    }
+
+}
